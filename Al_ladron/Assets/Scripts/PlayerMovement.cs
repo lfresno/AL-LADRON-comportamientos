a118d87b50372c22966792386d45f0697e9a4798
Lucía Fresno <lucia.fresno.olmeda@gmail.com>
2024-04-20 21:17:43 +0200

    updates in player movement script

--- Al_ladron/Assets/Scripts/PlayerMovement.cs
+++ Al_ladron/Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,14 @@ public class PlayerMovement : MonoBehaviour
     //code source : https://docs.unity3d.com/ScriptReference/CharacterController.Move.html 
 
     private CharacterController controller;
-    private Vector3 playerVelocity;
     
     [SerializeField]
     private float playerSpeed = 2.0f;
     private float speedIncrement = 1.0f;
     private float lastIncrement;
     private int playerTrack = 0;    //there are 4 tracks (0-3) in which the player can choose to run
-    private bool groundedPlayer;
-    private float gravityValue = -9.81f;
+
+    private bool lookingBack = false;
 
     private void Start()
     {
@@ -26,49 +25,78 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {   
-        Debug.Log(Time.time);
-        //speed is incremented every 0.5 seconds
-        //the increment in speed is greater at the beggining of the game, so that it gets difficult fast, but not too much
-        if(Time.time - lastIncrement > 0.5f) {
-            if(Time.time < 2.0f){
-            speedIncrement += 0.05f;
-            } else {
-                speedIncrement += 0.035f;
-            }
+        //if player reaches the end of the created path, they will be redirected to the beggining 
+        //this is only used in the prototype to test NPCs, but not in the final game
+        if(gameObject.transform.position.z >= 1400) {
+            controller.Move(new Vector3(0, 0, -1000.0f));
         }
+
         
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
+        if(Input.GetKeyDown(KeyCode.Space) || lookingBack) {
+            //if space is pressed, the player will look back to ckeck on the thieves. 
+            //in this moment, the player will stop moving and the camera will move so that the player can check on the animals (they will try to hide)
+
+            //this will throw a coroutine
+            speedIncrement -= 0.03f;
+            StartCoroutine(LookBack());
+
+        } else {
+            //speed is incremented every 0.5 seconds
+            //the increment in speed is greater at the beggining of the game, so that it gets difficult fast, but not too much
+            if(Time.time - lastIncrement > 0.02f) {
+                if(Time.time < 2.0f){
+                    speedIncrement += 0.1f;
+                } else {
+                    speedIncrement += 0.07f;
+                }
+
+                lastIncrement = Time.time;
+            }
+
         }
 
         //player will have a constant movement forward and will only be able to move left or right 
         //if the player uses left or right keys, they will move to the corresponding track, if possible
         if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
             if(playerTrack < 3) {
-                playerTrack++;
                 controller.Move(new Vector3(5.0f, 0, 0));
+                // gameObject.transform.position = new Vector3(playerTrack * 5, gameObject.transform.position.y, gameObject.transform.position.z);
+                playerTrack++;
             }
 
         } else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
             if(playerTrack > 0) {
-                playerTrack--;
                 controller.Move(new Vector3(-5.0f, 0, 0));
+                // gameObject.transform.position = new Vector3(playerTrack * 5, gameObject.transform.position.y, gameObject.transform.position.z);
+                playerTrack--;
             }
 
         }
 
-        Vector3 direction = new Vector3(0, 0, 1);
+        Vector3 move = new Vector3(0, 0, playerSpeed + speedIncrement);
 
-        controller.Move(direction * Time.deltaTime * (playerSpeed + speedIncrement));
+        controller.Move(move * Time.deltaTime);
 
-        if (direction != Vector3.zero)
+        if (move != Vector3.zero)
         {
-            gameObject.transform.forward = direction;
+            gameObject.transform.forward = move;
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        //if player couldn't move properly between tracks and ends up between two of them, move them to the one they were before
+        if(gameObject.transform.position.x % 5 != 0) {
+            //TODO!
+        }
+
+    }
+
+    private IEnumerator LookBack() {       
+        lookingBack = true;
+        float t = Time.realtimeSinceStartup;
+        Time.timeScale = 0f;
+        while(Time.realtimeSinceStartup - t < 3.0f){
+            yield return 0;
+        }
+        lookingBack = false;
+        Time.timeScale = 1.0f;
     }
 }
