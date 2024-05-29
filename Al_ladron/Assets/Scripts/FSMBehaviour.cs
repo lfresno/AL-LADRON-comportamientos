31fbde44a636256f64f0c4dfa4431f0fe67b710d
Lucía Fresno <lucia.fresno.olmeda@gmail.com>
2024-05-29 21:45:16 +0200

    fsm

--- Al_ladron/Assets/Scripts/FSMBehaviour.cs
+++ Al_ladron/Assets/Scripts/FSMBehaviour.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class FSMBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Player
+    public GameObject player;
+    public PlayerMovement playerMovement;
+    private Vector3 distanceToPlayer0;
+    private Vector3 distanceToPlayer;
+
+    //FSM
+    [SerializeField] private State state = State.Chase;
+
     void Start()
     {
-        
+        distanceToPlayer0 = transform.position - player.transform.position;
+        distanceToPlayer = distanceToPlayer0;
+        playerMovement = player.GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch(state) {
+            case State.Chase:
+
+                //if enemy is close enough, changes to STEAL state 
+                if(distanceToPlayer.z <= 1.5f) { 
+                    distanceToPlayer = distanceToPlayer0;
+                    state = State.Steal;
+                    break;
+                }
+
+                //if player looks back, change to HIDE state
+                if(playerMovement.lookingBack == true) {
+                    distanceToPlayer = distanceToPlayer0;
+                    state = State.Hide;
+                    break;
+                }
+
+                //chase player
+                //enemy gets progrisevely closer to the player
+                distanceToPlayer = distanceToPlayer + (new Vector3(0, 0, 0.01f));
+                transform.position = player.transform.position + distanceToPlayer;
+                break;
+
+            case State.Steal:
+                //steal
+                break;
+
+            case State.Hide:
+                //hide
+                break;
+                
+            default:
+                break;
+        }
     }
 }
+
+//STATES
+public enum State   //the NPC will chase the player, if they are close enough, it will steal them. If the player turns around to check on it, it will hide.
+{
+    Chase,
+    Steal,
+    Hide
+}
+
